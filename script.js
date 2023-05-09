9027966e1d877d24e8b48110ee1ad0508d60f582
alyersin <79478072+alyersin@users.noreply.github.com>
2023-05-09 23:51:47 +0300

    09052023

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 // //JS INTEGRATED GEOLOCATION
-const successCallback = (position) => {
-  let currPos = position;
-  console.log(currPos);
-  let {
-    coords: { latitude, longitude },
-  } = currPos;
-  console.log(latitude, longitude);
-};
+// const successCallback = (position) => {
+//   let currPos = position;
+//   console.log(currPos);
+//   let {
+//     coords: { latitude, longitude },
+//   } = currPos;
+//   console.log(latitude, longitude);
+// };
 
-const errorCallback = (error) => {
-  console.log(error);
-};
-navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+// const errorCallback = (error) => {
+//   console.log(error);
+// };
+// navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
 
 //GEOLOCATION & WEATHER API (JSON)
 
@@ -36,10 +36,18 @@ getCity.addEventListener("keydown", (e) => {
         const res1 = await axios.get(
           `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m,apparent_temperature,rain,snowfall,surface_pressure,cloudcover,windspeed_10m,winddirection_10m,windgusts_10m,uv_index,is_day&current_weather=true`
         );
+
         console.log(res1.data);
         let currWeather = res1.data;
+        let hourlyWeather = res1.data.hourly;
+
+        //CURRENT WEATHER
         const {
-          current_weather: { temperature: currTemp, windspeed: windspeed },
+          current_weather: {
+            temperature: currTemp,
+            windspeed: windSpeed,
+            winddirection: windDirection,
+          },
         } = currWeather;
 
         document.querySelector(
@@ -57,18 +65,61 @@ getCity.addEventListener("keydown", (e) => {
         }
 
         document.querySelectorAll(".temp").forEach((element) => {
-          element.innerText = `${currTemp}° C`;
+          element.innerText = `${currTemp}°`;
         });
+
+        //HOURLY WEATHER
+        const {
+          apparent_temperature: realFeel,
+          cloudcover: cloudCover,
+          is_day: dayNight,
+          time: time,
+        } = hourlyWeather;
+
+        const timeObj = time.reduce((acc, curr, index) => {
+          acc[curr] = `${realFeel[index]}, ${cloudCover[index]}`;
+          return acc;
+        }, {});
+        console.log(timeObj);
+
         getCity.value = "";
+
+        // <i class="bi bi-brightness-high"></i>
+        // <i class="bi bi-moon"></i>;
+
+        //WINDSPEED * DIRECTION
+
+        const windSp = document.querySelector(".wind-speed");
+        const windDir = document.querySelector(".wind-direction");
+
+        windSp.innerText = `${windSpeed} km/h`;
+
+        if (windDirection > 0 && windDirection < 45) {
+          windDir.innerText = "NNE";
+        } else if (windDirection > 45 && windDirection < 90) {
+          windDir.innerText = "ENE";
+        } else if (windDirection > 90 && windDirection < 135) {
+          windDir.innerText = "ESE";
+        } else if (windDirection > 135 && windDirection < 180) {
+          windDir.innerText = "SSE";
+        } else if (windDirection > 180 && windDirection < 225) {
+          windDir.innerText = "SSW";
+        } else if (windDirection > 225 && windDirection < 270) {
+          windDir.innerText = "WSW";
+        } else if (windDirection > 270 && windDirection < 315) {
+          windDir.innerText = "WNW";
+        } else if (windDirection > 315 && windDirection < 360) {
+          windDir.innerText = "NNW";
+        }
 
         //TIMEZONE API
         const res2 = await axios.get(
           `https://api.ipgeolocation.io/timezone?apiKey=dda0da9b52624e0c80f4972ad83865e2&lat=${lat}&long=${long}`
         );
         console.log(res2.data);
+
         let rawTime = res2.data.date_time;
         let currTime = rawTime.split("").splice(11, 5).join("");
-        console.log(currTime);
 
         let [hours, minutes] = currTime
           .split(":")
@@ -78,6 +129,21 @@ getCity.addEventListener("keydown", (e) => {
         } else {
           document.querySelector(".time").innerText = `${currTime} AM`;
         }
+
+        //LOOPING ARRAY FROM WEATHER API
+        let date = rawTime.split("").splice(0, 10).join("");
+        let hour = rawTime.split("").splice(11, 2).join("");
+        let dateTime = date.concat("T").concat(hour);
+
+        for (let key in timeObj) {
+          let splitKey = key.split("").splice(0, 13).join("");
+          // console.log(splitKey);
+          if (dateTime === splitKey) {
+            document.querySelector(".real-feel").innerText = `Real Feel ${
+              timeObj[key].split(",")[0]
+            }°`;
+          }
+        }
       } catch (e) {
         console.log("ERROR", e);
       }
@@ -85,6 +151,8 @@ getCity.addEventListener("keydown", (e) => {
     getLocation(getCity.value);
   }
 });
+
+//NEWS FEED API
 
 const newsOne = document.querySelector(".testNewsOne");
 const newsTwo = document.querySelector(".testNewsTwo");
@@ -98,13 +166,12 @@ const imgThree = document.querySelector(".imgThree");
 const imgFour = document.querySelector(".imgFour");
 const imgFive = document.querySelector(".imgFive");
 
-//NEWS FEED API
 const getNews = async () => {
   try {
     const res = await axios.get(
       "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=69b56f99050f4f98a6241abd7d29c8e7"
     );
-    console.log(res.data.articles);
+    // console.log(res.data.articles);
     const newsFeed = res.data.articles;
     newsOne.innerText = newsFeed[0].title;
     newsOne.setAttribute("href", res.data.articles[0].url);
