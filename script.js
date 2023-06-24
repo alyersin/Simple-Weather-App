e2d5a1d3dae9239b8dc39300ec5fe8751ce52e6f
alyersin <79478072+alyersin@users.noreply.github.com>
2023-06-24 18:35:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(showPosition);
+} else {
+  console.log("Geolocation is not supported by this browser.");
+}
+
+function showPosition(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+
+  console.log("Latitude: " + latitude);
+  console.log("Longitude: " + longitude);
+}
+
 //GEOLOCATION & WEATHER API (JSON)
 
 const getCity = document.querySelector("#enter-city");
